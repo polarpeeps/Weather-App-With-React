12eea3a55e982c94007cb9c992e9590f602200b2
Nabeel <ali31hasan.com>
2024-04-07 14:50:47 +0530

    v1

---
+++ weather/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./layouts/Navbar";
+import HeroSection from "./layouts/HeroSection"
+const App=()=>{
+    
+    return(
+        <div>
+        <Navbar />
+        <HeroSection />
+        </div>
+    )
+}
+export default App
